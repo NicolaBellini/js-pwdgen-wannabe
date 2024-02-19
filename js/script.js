4100266fa400b2a88d55069ef522b83f5b2f2bce
NicolaBellini <bellininicola4366@gmail.com>
2024-02-19 15:16:27 +0100

    [add] number prompt

--- js/script.js
+++ js/script.js
@@ -9,10 +9,12 @@ const completeName=name + ' ' + surname;
 
 const favColor=prompt("qual' è il tuo colore preferito, rosso o blu?");
 
+const number=prompt("qual' è il tuo numero fortunato?");
+
 document.getElementById('output').innerHTML=`
 <div class="${favColor}">
   <div>
-  Se ho capito bene ti chiami ${completeName} e il tuo colore preferito è il ${favColor}!
+  Se ho capito bene ti chiami ${completeName},  il tuo colore preferito è il ${favColor} e il tuo numero fortunato è il ${number}
   </div>
 </div>
 `;
