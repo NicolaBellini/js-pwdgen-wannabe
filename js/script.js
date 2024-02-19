ef38ab27d91930e820d715acda737412f20f5d4d
NicolaBellini <bellininicola4366@gmail.com>
2024-02-19 15:11:23 +0100

    [add] color choice and font

--- js/script.js
+++ js/script.js
@@ -7,6 +7,14 @@ const surname=prompt("qual' è il tuo cognome?")
 
 const completeName=name + ' ' + surname;
 
-const favColor=prompt("qual' è il tuo colore preferito?");
+const favColor=prompt("qual' è il tuo colore preferito, rosso o blu?");
+
+document.getElementById('output').innerHTML=`
+<div class="${favColor}">
+  <div>
+  Se ho capito bene ti chiami ${completeName} e il tuo colore preferito è il ${favColor}!
+  </div>
+</div>
+`;
 
 console.log(completeName, favColor);
